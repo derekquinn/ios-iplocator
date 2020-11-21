fdee0d95b9922d428bc7f471eee752297b28b904
derek quinn <dereq@Troy.local>
2020-11-21 05:52:52 -0500

    Added button to initiate location update

--- iplocator/ViewController.swift
+++ iplocator/ViewController.swift
@@ -11,16 +11,20 @@ import MapKit
 class ViewController: UIViewController {
     
     @IBOutlet var mapOfIPLocation: MKMapView!
- 
+    //   @IBOutlet var userEnteredIp: UITextField!
+    
     private var ipCoordinates: [Double] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func locateButtonPressed() {
         
         LocationService.getIPLocation(ip: Constant.michiganTestIp, locationCompletion: { userLocation in
             
             LocationHelper.updateLocation(latitude: userLocation[0], longitude: userLocation[1], mapOfIpLocation: self.mapOfIPLocation)
-        
+            
         })
     }
 }
